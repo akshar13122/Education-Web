546b348ceaba2ec70b7f2e4ccd07dfde26d6e7d2
Akshar <ak@gmail.com>
2025-03-12 23:46:13 +0530

    change in server.js

--- backend/Server.js
+++ backend/Server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",  
-  password: "root",  
+  password: "akshar2409",  
   database: "edu_users",  
 });
 
